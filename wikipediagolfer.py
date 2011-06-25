6d4eab11239a6843ffd38f01a4be4f780682ee29
Angus Fletcher <ethdem@gmail.com>
2011-06-25 13:05:27 -0300

    Changed file i/o operations to reduce memory use.

--- wikipediagolfer.py
+++ wikipediagolfer.py
@@ -3,7 +3,7 @@ wikipediagolfer.py
 
 Created by Angus Fletcher on 2011-06-20.
 
-Copyright (c) <year> <copyright holders>
+Copyright (c) 2011 Angus Fletcher
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -25,26 +25,29 @@ SOFTWARE.
 """
 
 import sys
+import os
 from xml.etree import ElementTree as etree
+from json.encoder import JSONEncoder as jencoder
 import re
 
-#temporary file hardcoded
-file_name = 'Wikipedia-dump.xml'
-
-def process_pages(root):
-	'''Runs through the xml tree and extracts the titles and links for each
+def process_pages(filename):
+	"""Runs through the xml tree and extracts the titles and links for each
 	page, returns a dictionary with keys equal to the pages and a list of
-	values which are strings of outgoing links.'''
-	page_dict = {}
+	values which are strings of outgoing links."""
 	
-	#Rip through the pages to process what we need.
-	for child in root:
-		if child.tag == '{http://www.mediawiki.org/xml/export-0.5/}page':
-			page_title = child.find('{http://www.mediawiki.org/xml/export-0.5/}title').text
-			#Right now we should pick one rev and use it, the dumps have only one so we can just get it
-			page_revision = child.find('{http://www.mediawiki.org/xml/export-0.5/}revision')
-			page_text = page_revision.find('{http://www.mediawiki.org/xml/export-0.5/}text').text
-			
+	encoder = jencoder()
+	
+	graph_file = open('graphfile.txt', 'w')
+	
+	parser = etree.iterparse(filename)
+	
+	graph_values = []
+	
+	for event, elem in parser:
+		if event == 'end' and elem.tag == '{http://www.mediawiki.org/xml/export-0.5/}title':
+			page_title = elem.text
+		if event == 'end' and elem.tag == '{http://www.mediawiki.org/xml/export-0.5/}text':
+			page_text = elem.text			
 			page_text = page_text.replace('\r', '').replace('\n', '')
 
 			#Now lets grab all of the outgoing links and store them in a list
@@ -64,9 +67,18 @@ def process_pages(root):
 			key_vals = set(key_vals)
 			key_vals = list(key_vals)
 			
-			page_dict[page_title] = key_vals
+			curr_pair = {page_title : key_vals }
 			
-	return page_dict
+			graph_values.append(encoder.encode(curr_pair))
+			
+			
+			if len(graph_values) > 1000:
+				write_string = '\n'.join(graph_values)
+				graph_file.write(write_string)
+				graph_values = []
+			
+	graph_file.close()
+	return
 	
 def strip_edges(graph):
 	"""Given a graph in the form of a dictionary with key being the vertex name
@@ -82,22 +94,15 @@ def strip_edges(graph):
 		values = [curr_value for curr_value in values if curr_value in list_keys]
 		graph[curr_key] = values
 		
-	return graph
-						
-				
+	return graph				
 			 		
 
 def main(argv=None):
-	try:
-		wikitree = etree.parse(file_name)
-	except IOError:
-		print 'Cannot parse' + file_name
+	process_pages('enwiki-latest-pages-articles.xml')
 	
-	root = wikitree.getroot()
+	#wiki_graph = strip_edges(wiki_graph)
 	
-	wiki_graph = process_pages(root)
-	
-	wiki_graph = strip_edges(wiki_graph)
+	print wiki_graph
 	
 	return 0
 
